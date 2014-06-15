f1ca353649d0a465f0cd7910c1a809fc611de1ee
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2014-06-15 22:49:55 +0400

    reuse switch-events-stmt for all clients

--- service/src/service/core.clj
+++ service/src/service/core.clj
@@ -21,33 +21,34 @@
     (.start (Thread. #(switch-loop esp-service x y 0)))))
 
 (defn current-state-handler [last-switch-events-stmt]
-    {:width width :height height
-     :switch-times (esp/pull-events last-switch-events-stmt)})
+  {:width width :height height
+   :switch-times (esp/pull-events last-switch-events-stmt)})
 
-(defn events-handler [esp-service request]
-  (let [stmt (esp/create-statement esp-service "select * from SwitchEvent")]
-    (http-kit/with-channel request channel
+(defn switch-events-broadcast-fn [channel & events]
+  (doseq [event events]
+    (println (sort event))
+    (http-kit/send! channel (pr-str event))))
+
+(defn events-handler [switch-events-stmt request]
+  (http-kit/with-channel request channel
+    (let [switch-listener
+          (esp/create-listener (partial switch-events-broadcast-fn channel))]
+
       (println "channel opened")
-      (http-kit/on-receive channel (fn [data]))
+      (esp/attach-listener switch-events-stmt switch-listener)
 
       (http-kit/on-close channel
-        (fn [status]
-          (.destroy stmt)
+        (fn [status] 
+          (esp/detach-listener switch-events-stmt switch-listener)
           (println "channel closed")))
-
-      (defn switch-listener [& events]
-        (doseq [event events]
-          (println (sort event))
-          (http-kit/send! channel (pr-str event))))
-
-      (esp/attach-listener stmt (esp/create-listener switch-listener))
-    )))
+)))
 
 (defn -main [& args]
   (let [esp-conf (esp/create-configuration [SwitchEvent])
         esp-service (esp/create-service "CrossroadsSimulator" esp-conf)
+        switch-events-stmt (esp/create-statement esp-service "select * from SwitchEvent")
         last-switch-events-stmt (esp/create-statement esp-service "select * from SwitchEvent.std:unique(x,y)")]
     (start-simulation esp-service width height)
     (start-web-service {:port 3000}
       (partial current-state-handler last-switch-events-stmt)
-      (partial events-handler esp-service))))
+      (partial events-handler switch-events-stmt))))
