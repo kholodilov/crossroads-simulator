a0e742c531e9e854e7373552bf5cdbea37073b4f
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2014-07-09 10:26:50 +0400

    data-source module

---
+++ data-source/project.clj
@@ -0,0 +1,10 @@
+(defproject data-source "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [ruiyun/tools.timer "1.0.1"]
+                 [com.novemberain/langohr "2.11.0"]]
+  :main data-source.core)
---
+++ data-source/src/data_source/core.clj
@@ -0,0 +1,10 @@
+(ns data-source.core
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.queue     :as lq]
+            [langohr.consumers :as lc]
+            [langohr.basic     :as lb]))
+
+(defn -main [& args]
+  (println "It works")
+  )
