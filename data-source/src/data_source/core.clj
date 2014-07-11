4f5a5b383bdd6658680bf19ed106e6a2068f7d20
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2014-07-11 10:12:40 +0400

    data source - use langohr for RMQ connectivity

--- data-source/src/data_source/core.clj
+++ data-source/src/data_source/core.clj
@@ -2,9 +2,16 @@
   (:require [langohr.core      :as rmq]
             [langohr.channel   :as lch]
             [langohr.queue     :as lq]
-            [langohr.consumers :as lc]
             [langohr.basic     :as lb]))
 
+(def default-exchange "")
+(def switch-events-queue "switch-events")
+
 (defn -main [& args]
-  (println "It works")
-  )
+  (let [conn (rmq/connect)
+        ch (lch/open conn)]
+    (lq/declare ch switch-events-queue :exclusive false)
+    (lb/publish ch default-exchange switch-events-queue "Hello!")
+    (rmq/close ch)
+    (rmq/close conn)
+  ))
