9ec84deaf071abd2b3c91b557f4e89b31b7d5b34
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2014-10-19 02:37:23 +0400

    finally made the simulation work

--- sumo-integration/src/sumo_integration/core.clj
+++ sumo-integration/src/sumo_integration/core.clj
@@ -3,15 +3,19 @@
            [de.tudresden.sumo.cmd Vehicle]))
 
 (defn -main [& args]
-  (let [conn (doto 
+  (let [step 300
+        step-length-seconds (str (/ step 1000.))
+        conn (doto 
                 (SumoTraciConnection. "/opt/sumo/bin/sumo-gui" "simulation/config.sumo.cfg")
-                (.addOption "step-length" "0.1")
+                (.addOption "step-length" step-length-seconds)
                 (.addOption "start" nil))]
     (.runServer conn)
-    (doseq [i (range 3600)]
+    (doseq [i (range 1 300)]
       (do
         (.do_timestep conn)
-        (.do_job_set conn (Vehicle/add (str "v" i) "car" "r1" 0 0 13.8 1))
+        (.do_job_set conn (Vehicle/add (str "v" i) "car" "r1" (* i step) 0 13.8 1))
+        (println i)
+        (Thread/sleep step)
       ))
     (println "close")
     (.close conn)
