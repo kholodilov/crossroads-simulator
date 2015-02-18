5c7f10364ba050c9b36d25304639bae0202c91a8
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2015-02-19 01:39:09 +0300

    network generator - generate full set of files for the network

--- sumo-integration/src/sumo_integration/generator.clj
+++ sumo-integration/src/sumo_integration/generator.clj
@@ -3,13 +3,21 @@
             [clostache.parser :as clostache]
             [clojure.java.io :as io]))
 
+(defn write-file [path content]
+  (with-open [w (io/writer path)]
+    (.write w content)))
+
+(defn copy-resources [dir & resources]
+  (doseq [resource resources]
+    (write-file (str dir "/" resource) (slurp (io/resource resource)))))
+
 (defn generate-network [sumo-home output-dir network-name & {:keys [width height grid-length attach-length] :as params}]
   (let [network-dir (str output-dir "/" network-name)
         netgenerate-file (str network-dir "/netgenerate.xml")
         netgenerate-params (merge params {:network-dir network-dir})
         netgenerate-config (clostache/render-resource "netgenerate.xml.template" netgenerate-params)]
     (sh "mkdir" "-p" network-dir)
-    (with-open [w (io/writer netgenerate-file)]
-      (.write w netgenerate-config))
+    (write-file netgenerate-file netgenerate-config)
     (sh (str sumo-home "/bin/netgenerate") "-c" netgenerate-file)
+    (copy-resources network-dir "config.sumo.cfg" "additional.xml" "routes.xml" "settings.xml")
 ))
