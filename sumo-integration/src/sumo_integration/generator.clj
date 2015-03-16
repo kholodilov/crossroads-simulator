3482843b8797b004b0a3a7d1dedaf94cbef4e81d
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2015-03-17 02:13:24 +0300

    generator - handle errors

--- sumo-integration/src/sumo_integration/generator.clj
+++ sumo-integration/src/sumo_integration/generator.clj
@@ -11,10 +11,16 @@
   (doseq [resource resources]
     (write-file (str dir "/" resource) (slurp (io/resource resource)))))
 
+(defn sh* [& cmd]
+  (let [{:keys [exit out err] :as sh-result} (apply sh cmd)]
+    (if (not= exit 0)
+      (throw (Exception. (str sh-result)))
+      (println out))))
+
 ; http://sumo.dlr.de/wiki/Simulation/Output/Lanearea_Detectors_(E2)
 ; http://sumo.dlr.de/wiki/TraCI/Lane_Area_Detector_Value_Retrieval
 (defn generate-e2 [sumo-home network-dir e2-length]
-    (sh (str sumo-home "/tools/output/generateTLSE2Detectors.py")
+  (sh* (str sumo-home "/tools/output/generateTLSE2Detectors.py")
         "-n" (str network-dir "/network.xml")
         "-l" (str e2-length)
         "-f" "1"
@@ -31,9 +37,9 @@
         netgenerate-params (merge params {:network-dir network-dir})
         netgenerate-config (clostache/render-resource "netgenerate.xml.template" netgenerate-params)]
 
-    (sh "mkdir" "-p" network-dir)
+    (sh* "mkdir" "-p" network-dir)
     (write-file netgenerate-file netgenerate-config)
-    (sh (str sumo-home "/bin/netgenerate") "-c" netgenerate-file)
+    (sh* (str sumo-home "/bin/netgenerate") "-c" netgenerate-file)
 
     (copy-resources network-dir "config.sumo.cfg" "vtypes.xml" "routes.xml" "settings.xml")
 
